80ee69fc96b094848604812d71908b87d57f4585
Elaanbari-Mostafa <animatricessiaap@gmail.com>
2022-04-10 15:28:00 +0000

    re name

--- Pegaxy Rent Bot.py
+++ Pegaxy Rent Bot.py
@@ -1,6 +1,7 @@
 import pyautogui,time,os,datetime
 
-time.sleep(5)
+time.sleep(2)
+print("start")
 while True:
     #if datetime.datetime.now().hour == 4:
       # os.system ('shutdown /s /t 10')
@@ -9,34 +10,34 @@ while True:
 
 
 
-    position = pyautogui.locateOnScreen(r'confermmeta.JPG', confidence=0.8)
+    position = pyautogui.locateOnScreen(r'img\confermmeta.JPG', confidence=0.8)
     if position != None:
         pyautogui.moveTo(position[0] + 25, position[1] + 25)
         pyautogui.click()
-    position = pyautogui.locateOnScreen(r'start.JPG', confidence=0.8)
+    position = pyautogui.locateOnScreen(r'img\start.JPG', confidence=0.8)
     if position != None:
          pyautogui.moveTo(position[0] + 25, position[1] + 25)
          pyautogui.click()
-    position = pyautogui.locateOnScreen(r'findanoder.JPG', confidence=0.8)
+    position = pyautogui.locateOnScreen(r'img\findanoder.JPG', confidence=0.8)
     if position != None:
          pyautogui.moveTo(position[0] + 25, position[1] + 25)
          pyautogui.click()
-    position = pyautogui.locateOnScreen(r'1st.JPG', confidence=0.9)
+    position = pyautogui.locateOnScreen(r'img\1st.JPG', confidence=0.9)
     if position != None:
         pyautogui.click()
         pyautogui.scroll(-50)
     time.sleep(1)
-    position = pyautogui.locateOnScreen(r'nextmatch.JPG', confidence=0.5)
+    position = pyautogui.locateOnScreen(r'img\nextmatch.JPG', confidence=0.5)
     if position != None:
         pyautogui.moveTo(position[0] + 25, position[1] + 25)
         pyautogui.click()
 
-    position = pyautogui.locateOnScreen(r'inderst.JPG', confidence=0.8)
+    position = pyautogui.locateOnScreen(r'img\inderst.JPG', confidence=0.8)
     if position != None:
         pyautogui.moveTo(position[0] + 10, position[1] + 20)
         pyautogui.click()
         time.sleep(1)
-        position = pyautogui.locateOnScreen(r'url.JPG', confidence=0.8)
+        position = pyautogui.locateOnScreen(r'img\url.JPG', confidence=0.8)
         if position != None:
             pyautogui.moveTo(position[0] + 15, position[1] + 20)
             pyautogui.click()
